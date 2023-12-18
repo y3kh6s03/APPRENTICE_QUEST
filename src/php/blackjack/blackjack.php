04eb534d52ea9f402766270a9e2317656e95a474
yosuke026 <y3kh6s03@gmail.com>
2023-12-18 22:03:11 +0900

    scoreを未実装

---
+++ src/php/blackjack/blackjack.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "game.php";
+require_once "card.php";
+
+use card\Card;
+use game\Game;
+
+
+function blackjack()
+{
+    $blackjack = new Game;
+    $playerCards = new Card;
+    $dealerCards = new Card(false);
+
+    $blackjack->start();
+    $playerCards->drewCards();
+    $dealerCards->drewCards();
+    $blackjack->info($playerCards);
+    $blackjack->info($dealerCards);
+}
